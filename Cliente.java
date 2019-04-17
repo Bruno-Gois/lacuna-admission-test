dd8f067dad29269a1e5c9b6328eee5423edb0511
bruno.gois <brunovgois@gmail.com>
2019-04-16 23:57:06 -0300

    processo p descobrir chave incompleto

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,5 @@
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.IOException;
@@ -6,14 +7,14 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 public class Cliente {
-    
+
     Socket L314;
     DataInputStream dataInput;
     DataOutputStream dataOutput;
     String token = "caff5e93-cae0-4b85-8466-0d2c8f8c5323";
 
     Cliente(String endereco, int porta) {
-        initCliente(endereco, porta);        
+        initCliente(endereco, porta);
     }
 
     private void initCliente(String endereco, int porta) {
@@ -27,65 +28,84 @@ public class Cliente {
 
             escreverMensagem(token, dataOutput);
             
-            lerMensagemCript(dataInput);
+            lerUTF(dataInput);
+
+            escreverMensagem("tell me more", dataOutput);
+
+            ChaveDecodificacao(lerMensagemCript(dataInput));
             
-            // System.out.println("TESTES: 1333 p byte -");
-            // Integer bla = 1333;
-            // System.out.println(bla.byteValue());
-            // escreverMensagem("tell me more", dataOutput);
-            
-            // lerMensagemCript(dataInput);
-            
-            //  escreverMensagem("stop", dataOutput);
-        }
-        catch (Exception e) {
+            // escreverMensagem("stop", dataOutput);
+        } catch (Exception e) {
             System.err.println("erroInit: " + e.toString());
         }
     }
+    
+    private void ChaveDecodificacao(byte[] msgCript) {
+        byte[] vader = {86,97,100,101,114}; //Vader in ASCII
+        int chave;
+        int chaveTemp;
+        int nroInteracoes = 0;
 
-    private void lerMensagemCript(DataInputStream dataInput) {
-        try{
-            int len;
-            byte[] buffer = new byte[1024];
-            int padraoMsgImperio = 0;
+        // for(int i = 0; i < (msgCript.length-vader.length); i++) {
+        //     while() {
+        //         chave = vader[i] ^ msgCript[i];
+        //         chaveTemp = vader[i++] ^ msgCript[i++];
+        //         System.out.println("CHAVE " + chave);
+        //         System.out.println("TEMP " + chaveTemp);
 
-            ArrayList<Byte> tamanhoMsgLida = new ArrayList<>();
-            ArrayList<Byte> msgLida = new ArrayList<>();
-            byte checkSumByte = -1;
+        //     }
+        // }
+    }
 
-            while ((len = dataInput.read(buffer)) > 0){ 
-            
+
+    private void lerUTF(DataInputStream dataInput) {
+        byte[] byteMsg = lerMensagemCript(dataInput);
+
+        System.out.println(new String(byteMsg, StandardCharsets.UTF_8));
+    }
+
+    private byte[] lerMensagemCript(DataInputStream dataInput) {
+        int len;
+        byte[] buffer = new byte[1024];
+        int padraoMsgImperio = 0;
+
+        ArrayList<Byte> tamanhoMsgLida = new ArrayList<>();
+        ArrayList<Byte> msgLida = new ArrayList<>();
+        byte checkSumByte = -1;
+        try {
+            while ((len = dataInput.read(buffer)) > 0) {
+
                 int i = 0;
-                while(i < len) {  
-                    //System.out.print(buffer[i] + " ");
-                    if(padraoMsgImperio == 0)
+                while (i < len) {
+                    // System.out.print(buffer[i] + " ");
+                    if (padraoMsgImperio == 0)
                         tamanhoMsgLida.add(buffer[i]);
-                    if(padraoMsgImperio == 1)
+                    if (padraoMsgImperio == 1)
                         msgLida.add(buffer[i]);
-                    if(padraoMsgImperio == 2)
+                    if (padraoMsgImperio == 2)
                         checkSumByte = buffer[i];
-                    
+
                     i++;
                 }
-                //de acordo com o padrao de msg do imperio, o loop so ira rodar 3 vezes, capturando na primeira iteracao
-                //o tamanho, na segunda a msg em si, e na terceira o checksum
-                if(padraoMsgImperio > 1)
+                // de acordo com o padrao de msg do imperio, o loop so ira rodar 3 vezes,
+                // capturando na primeira iteracao
+                // o tamanho, na segunda a msg em si, e na terceira o checksum
+                if (padraoMsgImperio > 1)
                     break;
 
                 padraoMsgImperio++;
             }
-            if(analisarChecksum(msgLida, tamanhoMsgLida ,checkSumByte)) {
-                System.out.println("Tamanho msg: " + tamanhoMsgLida);
-                System.out.println("Mensagem: " + msgLida);
-                System.out.println("Checksum: " + checkSumByte);
-            }
-            else {
-                System.out.println("Erro na leitura! repetir?");
-            }
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("erroLeitura: " + e.toString());
+        }
+
+        if (analisarChecksum(msgLida, tamanhoMsgLida, checkSumByte)) {
+            System.out.println(msgLida);
+            return converterParaPrimitivo(msgLida.toArray(new Byte[0]));
+        } else {
+            System.out.println("Erro no checksum!");
+            return null;
         }
     }
 
@@ -93,32 +113,34 @@ public class Cliente {
         try {
             dataOutput.write(msg.getBytes());
             dataOutput.flush();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("erroEscrita: " + e.toString());
         }
     }
 
-    private void lerMensagemUTF(DataInputStream dataInput) {
-        try {
-            System.out.println(dataInput.readUTF());
-        } catch (Exception e) {
-            System.err.println("erroLeituraUTF: " + e.toString());
-        }     
+    private byte[] converterParaPrimitivo(Byte [] arrayRecebido) {
+        byte[] bytes = new byte[arrayRecebido.length];
+
+        int j=0;
+        // Unboxing Byte values. (Byte[] to byte[])
+        for(Byte by: arrayRecebido)
+            bytes[j++] = by.byteValue();
+
+        return bytes;
     }
 
     private boolean analisarChecksum(ArrayList<Byte> msgLida, ArrayList<Byte> tamanhoMsg, byte checksum) {
-    
+
         Integer somatorio = 0;
 
-        for(Byte i: msgLida) {
+        for (Byte i : msgLida) {
             somatorio = somatorio + i;
-        } 
-        for(Byte i: tamanhoMsg) {
+        }
+        for (Byte i : tamanhoMsg) {
             somatorio = somatorio + i;
         }
 
-        if(somatorio.byteValue() == checksum) {
+        if (somatorio.byteValue() == checksum) {
             return true;
         }
         return false;

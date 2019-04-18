48d174e7e6990f3e08fa991e722c085941a80be9
bruno.gois <brunovgois@gmail.com>
2019-04-18 01:19:46 -0300

    mudar token

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
         public static void main(String[] args) {
-        new Cliente("lacuna-admission.brazilsouth.cloudapp.azure.com", 21829);
+        new Cliente("lacuna-admission.brazilsouth.cloudapp.azure.com", 21959);
     }
 }
